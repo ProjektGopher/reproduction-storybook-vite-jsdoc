e12358e48f891387c0fa65815f9e9474ff0f0d2e
Len Woodward <Len@ProjektGopher.com>
2022-10-15 23:46:39 -0700

    remove unused prop

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -9,8 +9,6 @@ module.exports = {
   async viteFinal(config) {
     // Merge custom configuration into the default config
     return mergeConfig(config, {
-      // Use the same "resolve" configuration as your app
-      resolve: (await import('../vite.config.js')).default.resolve,
       // Add dependencies to pre-optimization
       optimizeDeps: {
         include: ['vue'],
